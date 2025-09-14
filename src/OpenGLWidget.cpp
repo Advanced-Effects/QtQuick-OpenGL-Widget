786cf538780ea72b47b205ea11c3f3236013f8e9
kaixoo12 <208606493+kaixoo12@users.noreply.github.com>
2025-09-15 00:46:30 +0200

    feat: implement more code
    - gl code - gl resize code

--- src/OpenGLWidget.cpp
+++ src/OpenGLWidget.cpp
@@ -1,8 +1,67 @@
 #include "OpenGLWidget.h"
 
-QuickGLWidget::QuickGLWidget(QObject *parent = nullptr) {};
+#include <GLFW/glfw3.h>
+#include <QPoint>
 
-QuickGLWidget::onWidgetResize(int x,
-                              int y,
-                              int width,
-                              int height) {};
+QuickGLWidget::QuickGLWidget(QObject *parent = nullptr)
+        : QQuickWidget(parent) {};
+
+QRect QuickGLWidget::widgetGeometry() {
+        // Map the widget's top-left corner to global (screen) coordinates
+        QPoint topLeft = mapToGlobal(QPoint(0, 0));
+        QSize size = size();
+
+        return QRect(topLeft, size);
+};
+
+// Returns 'false' if initialization goes wrong
+bool QuickGLWidget::initializeGL() {
+        glfwInit();
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        #ifdef __APPLE__
+                glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        #endif
+
+        return glfwInit(); // 'false' if gone wrong
+};
+GLFWwindow* QuickGLWidget::createGLWindow() {
+        auto window = glfwCreateWindow(500, 500, "", glfwGetPrimaryMonitor(), nullptr);
+
+        if (!window) {
+                glfwTerminate();
+                return nullptr;
+        }
+
+        return window;
+};
+int QuickGLWidget::mainWindowLoop() {
+        if (m_glContext == nullptr) {
+                return EXIT_FAILURE;
+        }
+
+        // Runs until the window is destroyed
+        while (!glfwWindowShouldClose(m_glContext)) {
+                // It's okay to draw
+                paintThisFrame();
+
+                // 'Prepare' next frame
+                glfwSwapBuffers(m_glContext);
+                glfwPollEvents();
+        }
+
+        glfwDestroyWindow(m_glContext);
+
+        return 0;
+};
+
+void QuickGLWidget::resizeGLWindow(int x,
+                                   int y,
+                                   int width,
+                                   int height)
+{
+        glfwSetWindowPos(m_glContext, x, y);
+        glfwSetWindowSize(m_glContext, width, height);
+};
--- src/OpenGLWidget.h
+++ src/OpenGLWidget.h
@@ -1,9 +1,5 @@
 #pragma once
 
-#include <QObject>
-
-#include <GLFW/glfw3.h>
-
 // 1. Include libraries properly
 // 2. Find out how to make a custom QtQuick widget using only C++
 // 3. Initialise OpenGL, create and show window.
@@ -11,19 +7,48 @@
 // 5. Subdelegate rendering (virtual function)
 // 6. Input processing
 
-class QuickGLWidget {
+#include <QSize>
+#include <QRect>
+
+#include <QObject>
+#include <QQuickWidget>
+
+#include <GLFW/glfw3.h>
+
+// This is a widget which contains an OpenGL context
+//
+// - We create an external OpenGL window
+// - We resize the window to always cover the widget's area.
+class QuickGLWidget : public QQuickWidget {
+        Q_OBJECT
+        QML_ELEMENT
 public:
         QuickGLWidget(QObject *parent = nullptr);
         ~QuickGLWidget() = default;
 
-        virtual void onDrawNextFrame();
+        // Is run every frame
+        // Call gl- functions here
+        virtual void paintThisFrame();
 
 private:
+        /* ===== Qt ===== */
+
+        // Returns the absolute size and position of the widget
+        //
+        // Use .topLeft() to get the position in absolute (screen) terms.
+        // Use .size() to get the widget's size.
+        QRect widgetGeometry();
+
+        /* ===== OpenGL ===== */
         GLFWwindow *m_glContext;
+
+        bool initializeGL();
+        GLFWwindow* createGLWindow();
+        int mainWindowLoop();
 
         // Make sure the OpenGL window always covers
         // the same area as the QuickGLWidget.
-        void onWidgetResize(int x,
+        void resizeGLWindow(int x,
                             int y,
                             int width,
                             int height);
